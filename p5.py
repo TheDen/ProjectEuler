00bdd8f7ff7ba99e5fa97bfd1a28da9f62280e3a
Denis Khoshaba <deniskhoshaba@Denis.local>
2016-02-22 13:28:06 +1100

    problem 5

---
+++ p5.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+num = 380
+#div=[2,3,5,7,8,9]
+div=[7,8,9,11,13,16,17]
+div = div[::-1]
+alen = len(div)
+j = 0
+k = 0
+while True:
+
+        for i in range(0,alen):
+            if num%div[i]==0: 
+                j = j+1
+            else:
+                break
+                
+        if j == alen:
+            print num
+            break
+        j = 0
+        num = num+380
+   
+
+    
